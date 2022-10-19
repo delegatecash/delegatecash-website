dad388e03280db5d0ff6695af840659962bebf50
ryeshrimp <ryeshrimp@protonmail.com>
2022-10-19 14:49:14 -0400

    Add BlurContainer component

---
+++ src/design-system/BlurContainer.svelte
@@ -0,0 +1,9 @@
+<div class="backdrop-blur-sm bg-black/20">
+  <slot />
+</div>
+
+<style lang="postcss">
+  div {
+    @apply fixed w-full h-full left-0 top-0 z-50 backdrop-blur-sm bg-black/20;
+  }
+</style>
